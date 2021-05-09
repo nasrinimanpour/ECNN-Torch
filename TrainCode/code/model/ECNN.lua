cffa913a4d8796043d7d93bd0c3157031396a389
nasrinimanpour <43720872+nasrinimanpour@users.noreply.github.com>
2021-05-09 22:21:35 +0430

    Update ECNN.lua

--- TrainCode/code/model/ECNN.lua
+++ TrainCode/code/model/ECNN.lua
@@ -12,19 +12,22 @@ local function createModel(opt)
     actParams.u = opt.u
     actParams.alpha = opt.alpha
     actParams.negval = opt.negval
-  
+
 
     function addLayer(model, nChannels, opt)
        if opt.optMemory >= 3 then
           model:add(nn.DenseConnectLayerCustom(nChannels, opt))
        else
-          model:add(DenseConnectLayerStandard(nChannels, opt))     
+          model:add(DenseConnectLayerStandard(nChannels, opt))
        end
     end
     function DenseConnectLayerStandard(nChannels, opt)
        local net = nn.Sequential()
-       net:add(nn.SpatialConvolution(nChannels, opt.growthRate, 3, 3, 1, 1, 1, 1))
+       net:add(nn.SpatialConvolution(nChannels, 1024, 3, 3, 1, 1, 1, 1))
        net:add(nn.ReLU(true))
+net:add(nn.SpatialConvolution(1024, 32, 3, 3, 1, 1, 1, 1))
+       net:add(nn.ReLU(true))
+
        return nn.Sequential()
           :add(nn.Concat(2)
              :add(nn.Identity())
@@ -32,89 +35,28 @@ local function createModel(opt)
     end
     local function addDenseBlock(model, nChannels, opt, N)
        -- opt.growthRate: output feature number of each conv layer
-       for i = 1, N do 
+       for i = 1, N do
           addLayer(model, nChannels, opt)
-          nChannels = nChannels + opt.growthRate
+          nChannels = nChannels + 32
        end
        return nChannels
     end
-    function buildDenseBlocks(model, opt)
-       local scaleRes = (opt.scaleRes and opt.scaleRes ~= 1) and opt.scaleRes or false
-       local nDenseBlock = opt.nDenseBlock
-       local nDenseConv  = opt.nDenseConv
-       for i = 1, nDenseBlock do
-          local nChannels = opt.nFeat
-          local oneBlock = seq()
-          nChannels = addDenseBlock(oneBlock, nChannels, opt, nDenseConv)
-          oneBlock:add(nn.SpatialConvolution(nChannels, opt.nFeat, 1,1, 1,1, 0,0))
-          --oneBlock:add(nn.ReLU(true))
-          if scaleRes then
-              oneBlock:add(nn.MulConstant(opt.scaleRes, opt.ipMulc))
-          end
-          if opt.addBlockSkip then
-             addSkip(oneBlock)
-          end
-          model:add(oneBlock)
-       end
-       --return nChannels
-    end
     -- build one DenseBlock
-    function buildDenseBlockUnit(model, opt)
+    function buildDenseBlockUnit(opt)
        local scaleRes = (opt.scaleRes and opt.scaleRes ~= 1) and opt.scaleRes or false
        local nDenseConv  = opt.nDenseConv -- conv layer number in one dense block
        local nChannels = opt.nFeaSDB        -- input feature number opt.growthRate
        local BlockUnit = nn.Sequential()
        nChannels = addDenseBlock(BlockUnit, nChannels, opt, nDenseConv)
        -- local feature fusion via 1x1 conv layer
-       BlockUnit:add(nn.SpatialConvolution(nChannels, opt.nFeaSDB, 1,1, 1,1, 0,0))
-       --BlockUnit:add(nn.ReLU(true))
-       --if scaleRes then
-       --   BlockUnit:add(nn.MulConstant(opt.scaleRes, opt.ipMulc))
-       --end
-       -- add local skip connection
-       if opt.addBlockSkip then
-          --print('add local skip')
-          model:add(addSkip(BlockUnit, true))
-       else
-          model:add(BlockUnit)
-       end
-       
-       return model
+       BlockUnit:add(nn.SpatialConvolution(nChannels, opt.conv_1x1_count, 1,1, 1,1, 0,0))
+       return BlockUnit
     end
-    -- build dense blocks
-    function buildRecursiveDenseBlocks(DenseBlockUnit, nDenseBlockAdd)
-       local N = nDenseBlockAdd
-       local blockGrow = nn.Sequential()
-       if 1 == N then
-          blockGrow = buildDenseBlockUnit(blockGrow, opt)
-          blockGrow:add(
-              nn.Concat(2)
-              :add(nn.Identity())
-              :add(DenseBlockUnit)
-          )
-       else
-          blockGrow = buildDenseBlockUnit(blockGrow, opt)
-          blockGrow:add(
-              nn.Concat(2)
-              :add(nn.Identity())
-              :add(buildRecursiveDenseBlocks(DenseBlockUnit, N - 1))
-          )
-       end
-       return blockGrow
-    end
-    
-    local DenseBlockUnit = seq()
-    local body = nn.Sequential()
+ local body = nn.Sequential()
     body:add(nn.SpatialConvolution(opt.nFeat, opt.nFeaSDB, 3,3, 1,1, 1,1))
-    DenseBlockUnit = buildDenseBlockUnit(DenseBlockUnit, opt)
-    bodyDenseBlocks = buildRecursiveDenseBlocks(DenseBlockUnit, opt.nDenseBlock-1)
-    body:add(bodyDenseBlocks)
-    local nFeatTransit = opt.nFeaSDB*(opt.nDenseBlock)
-    -- global feature fusion via 1x1 conv layer
-    body:add(nn.SpatialConvolution(nFeatTransit, opt.nFeat, 1,1, 1,1, 0,0))
-    -- global conv
-    body:add(conv(opt.nFeat,opt.nFeat, 3,3, 1,1, 1,1))
-    -- feature extraction
+    body:add(buildDenseBlockUnit(opt))
+        body:add(nn.SpatialConvolution(opt.conv_1x1_count, opt.nFeaSDB, 3, 3, 1, 1, 1, 1))
+
     ret = seq():add(conv(opt.nChannel,opt.nFeat, 3,3, 1,1, 1,1))
     if opt.globalSkip then
         -- global skip connection
@@ -125,7 +67,14 @@ local function createModel(opt)
     ret:add(upsample_wo_act(opt.scale[1], opt.upsample, opt.nFeat))
         :add(conv(opt.nFeat,opt.nChannel, 3,3, 1,1, 1,1))
     print(ret)
+
+    params, gradParams = ret:getParameters()
+    print(params:size(1))
+    print(gradParams:size(1))
+
     return ret
-    
+
 end
 return createModel
+
+                      
